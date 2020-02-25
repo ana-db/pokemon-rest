b7bc5532d65e1c51ef2eb0c2291f1d9a8e680fa2
ana-db <anadiezbravo@outlook.com>
2020-02-25 09:24:25 +0100

    test constructor pokemon vacío

---
+++ src/test/java/com/ipartek/formacion/model/pojo/PokemonTest.java
@@ -0,0 +1,44 @@
+package com.ipartek.formacion.model.pojo;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PokemonTest {
+	
+	Pokemon nuevoPokemon = new Pokemon();
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		/* comprobamos que al crear un pokemon, su id es 0, su nombre e imagen
+		 * son cadenas de texto vacías y su array de habilidades está vacío
+		*/
+		assertEquals(nuevoPokemon.getId(), 0); 
+		assertEquals(nuevoPokemon.getNombre(), "");
+		assertEquals(nuevoPokemon.getImagen(), "");
+		assertEquals( nuevoPokemon.getHabilidades().size(), 0 );
+	}
+
+}
